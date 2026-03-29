40e0ff3c541a6430019d4a788cedb2f212cd8224
Pete Johns <paj+github@johnsy.com>
2026-03-30 10:42:53 +1100

    fix(ssh): parse destination with ssh -G and harden set_color awk
    Use OpenSSH's ssh -G so options and remote commands do not corrupt host/user.
    Replace awk regex interpolation with -v and index() to avoid glob and regex errors.

--- color-ssh.plugin.zsh
+++ color-ssh.plugin.zsh
@@ -2,6 +2,71 @@ set_title() {
     echo -ne "\033]0;${@}\007"
 }
 
+# Prefer OpenSSH's parser: same argv as a real ssh, including options and a remote command.
+_color_ssh_parse_destination() {
+    local out
+    out=$(command ssh -G "$@" 2>/dev/null) || return 1
+    [[ -n "$out" ]] || return 1
+    remote_user=$(print -r "$out" | command awk '/^user / { print $2; exit }')
+    remote_host=$(print -r "$out" | command awk '/^hostname / { print $2; exit }')
+    [[ -n "$remote_host" ]] || return 1
+    remote_host="${remote_host%%.*}"
+}
+
+# When ssh -G is unavailable, use the first non-option argument as the destination.
+_color_ssh_parse_destination_fallback() {
+    local -a args=("$@")
+    local i=1
+    local arg
+    while (( i <= $#args )); do
+        arg=$args[i]
+        if [[ $arg == -- ]]; then
+            (( ++i ))
+            continue
+        fi
+        if [[ $arg != -* ]]; then
+            _color_ssh_split_user_host "$arg"
+            remote_host="${remote_host%%.*}"
+            return 0
+        fi
+        case $arg in
+            -[bcDeEeFfIiJLlmOopQRWw])
+                if [[ ${#arg} -eq 2 ]]; then
+                    (( i += 2 ))
+                else
+                    (( ++i ))
+                fi
+                ;;
+            -o)
+                (( i += 2 ))
+                ;;
+            -o*)
+                (( ++i ))
+                ;;
+            *)
+                (( ++i ))
+                ;;
+        esac
+    done
+    if (( i <= $#args )); then
+        _color_ssh_split_user_host "$args[i]"
+        remote_host="${remote_host%%.*}"
+        return 0
+    fi
+    return 1
+}
+
+_color_ssh_split_user_host() {
+    local dest="$1"
+    if [[ "$dest" == *@* ]]; then
+        remote_user="${dest%%@*}"
+        remote_host="${dest#*@}"
+    else
+        remote_user=$(whoami)
+        remote_host="$dest"
+    fi
+}
+
 set_color() {
     type osascript &>/dev/null
     if [[ 0 != $? ]]; then
@@ -16,19 +81,26 @@ set_color() {
         return
     fi
 
-    host=${1}
-    color=$(awk -F':' '$1 ~ /'${host}'/ { print $2; exit }' ${color_file})
+    local host=${1}
+    local color
+    color=$(
+        command awk -F':' -v h="$host" '
+            $1 == h || index($1, h ".") == 1 { print $2; exit }
+        ' "$color_file"
+    )
     if [[ ! -z ${color} ]]; then
         osascript -e 'tell application "Terminal" to set current settings of selected tab of window 1 to (first settings set whose name is "'${color}'")'
     fi
 }
 
 ssh() {
-    remote_host=${${@##*@}%%.*}
-    if [[ $@ == *@* ]]; then
-        remote_user=${${@##* }%%@*}
-    else
-        remote_user=$(whoami)
+    local remote_user remote_host
+
+    if ! _color_ssh_parse_destination "$@"; then
+        _color_ssh_parse_destination_fallback "$@" || {
+            remote_user=$(whoami)
+            remote_host=${$(hostname)%%.*}
+        }
     fi
 
     set_title "${remote_user}@${remote_host}"
@@ -41,4 +113,3 @@ ssh() {
     set_title "$(whoami)@${$(hostname)%%.*}"
     set_color "Default"
 }
-
